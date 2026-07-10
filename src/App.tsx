48799b266f11ec0658c2e0670d7f39ddc2ac86d3
sugudon <sugufloyd01@gmail.com>
2026-07-11 04:07:40 +0530

    Test auto deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./stories/Button";
 function App() {
   return (
     <div>
-      <h1>Learning React</h1>
+      <h1>Auto Deploy Test</h1>
       <p>React + Vite</p>
       <Button label="Button" onClick={ () => console.log("Button clicked")}></Button>
     </div>
